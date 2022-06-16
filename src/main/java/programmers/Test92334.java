a683eee854eb50c7990df8e72fb3e231d5e77710
comecome2 <comecome2@glowmee.com>
2022-06-17 00:41:33 +0900

    1.프로그래머스 문제 풀기전에 졸려서 일단 이해한것만 주석으로 남김

---
+++ src/main/java/programmers/Test92334.java
@@ -0,0 +1,26 @@
+//package programmers;
+
+//https://programmers.co.kr/learn/courses/30/lessons/92334?language=java
+//
+//
+//muzi :  frodo, neo (2)
+//frodo : neo, (1)
+//apeach: frodo(muzi 신고해서), muzi  (1)
+//neo : (0)
+public class Test92334 {
+
+
+    public static void main(String[] args) {
+
+        String[] id_list = {"muzi", "frodo", "apeach", "neo"};
+
+        String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
+
+    }
+
+    public static int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = {};
+        return answer;
+    }
+
+}
